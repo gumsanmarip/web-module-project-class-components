b4d64e6639fdc36183d87cd6a9e6ac7d13b70b2d
gumsanmarip <gumsanmarip@gmail.com>
2022-02-07 17:10:18 -0500

    start

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { render } from 'react-dom';
+
+import Todo from './Todo';
+import TodoList from './TodoList';
+import Form from './Form';
+
+import './styles/reset.css';
+import './styles/styles.css';
+
+const todo = 
+[
+  {
+    name: 'Organize Garage',
+    id: 1528817077286, // could look different, you could use a timestamp to generate it
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 
 export default class App extends React.Component {
+
+
+
+
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo List: MVP</h1>
+
       </div>
     )
   }
